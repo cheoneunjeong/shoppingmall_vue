3a6cab9d9d9f4a6fa65f6dd13cf3b9caaa47b93d
cheoneunjeong <0102skyjung@naver.com>
2022-01-26 00:53:02 +0900

    create kakaoLogin/Logout

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Login from '../views/Login'
+import OAuth from '../views/OAuth'
 
 Vue.use(VueRouter)
 
@@ -17,7 +19,17 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/auth',
+    name: 'OAuth',
+    component: OAuth
+  },
 ]
 
 const router = new VueRouter({
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,47 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    token: ''
   },
   mutations: {
+    set_token(state, data) {
+      state.token = data
+    }
   },
   actions: {
+    kakaoLogin({ commit }, payload) {
+      return new Promise((resolve, reject) => {
+        axios.get('http://localhost:9010/api/public/kakaologin', { params: { code: payload } })
+          .then(Response => {
+            console.log(Response.data)
+            if (Response.data != null) {
+              axios.defaults.headers.common['Authorization'] = `Bearer ${Response.data}`
+              localStorage.setItem("token", Response.data)
+              commit('set_token', Response.data)
+            }
+          })
+          .catch(Error => {
+            console.log('kakaoLogin_error')
+          })
+      })
+    },
+    kakaoLogout({ commit, state }) {
+      return new Promise((resolve, reject) => {
+        axios.get('http://localhost:9010/api/public/kakaologout', { params: { code: state.token } })
+          .then(Response => {
+            console.log(Response.data)
+          })
+          .catch(Error => {
+            console.log('kakaoLogout_error')
+          })
+      })
+    },
+
   },
   modules: {
   }
